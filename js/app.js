d54242fbf53260de6bec2012d5198e94f6e2b636
Jamie Skinner <jamie@ninthmind.com>
2017-03-29 20:31:13 -0500

    added forces

--- js/app.js
+++ js/app.js
@@ -53,3 +53,108 @@ group.selectAll('.bubbles')
           }).on('mouseenter', (d)=>{
             value.text(d.r)
           })
+
+//three
+let three = d3.select('#svg-3')
+three.attr('height', 50)
+let colorScale = d3.scaleLinear()
+                    .domain([0, numBubbles])
+                    .range(['red', 'blue'])
+
+var g = three.append('g').attr('class','bubble-group')
+g.selectAll('.bubbles')
+          .data(data)
+          .enter()
+          .append('circle')
+          .attrs({
+            r: 10,
+            fill: (d,i) => colorScale(i),
+            cx: (d,i) => (maxSize + 2) * i ,
+            cy: '50%'
+          })
+
+//four
+let four = d3.select('#svg-4')
+four.attr('height', 100)
+
+let nodes = four.selectAll('.bubbles')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attrs({
+              r: 10,
+              fill: (d,i) => colorScale(i),
+              cx: (d,i) => (maxSize + 2) * i ,
+              cy: '50%'
+            })
+let dir = 1
+function step(){
+  setTimeout(()=>{
+    window.requestAnimationFrame(()=>{
+      nodes.transition()
+      .duration(1000)
+      .delay((d,i) => 50 * i)
+      .attr('cy', () => dir ? '90%' : '10%')
+      dir ^= 1
+      step()
+    })
+  }, 1000)
+}
+step()
+
+//five
+
+var nodes2;
+let five = d3.select('#svg-5')
+let width = 400
+five.attrs({
+  width: width,
+  height: h
+})
+draw()
+let sim = d3.forceSimulation()
+            .force("charge", d3.forceManyBody().strength(5))
+            .force('collide', d3.forceCollide(d => d.r + 5))
+            .force("center", d3.forceCenter(width / 2, h / 2))
+            .nodes(data)  // <= important
+
+
+sim.on('tick', tick)
+
+// FUNCTIONS
+function tick(){
+  nodes2.attrs({
+    cx: d => d.x,
+    cy: d => d.y
+  })
+}
+
+function draw(){
+  nodes2 = five.selectAll('.node')
+              .data(data).enter().append('circle').attrs({
+                r: d => d.r,
+                fill: (d,i) => colorScale(i),
+                x: d => Math.random() * width,
+                y: d => Math.random() * h,
+              })
+              .call(d3.drag()
+                      .on("start", dragstarted)
+                      .on("drag", dragged)
+                      .on("end", dragended))
+}
+
+// NODE DRAG LISTENERS
+function dragstarted(d) {
+  if (!d3.event.active) sim.alphaTarget(0.3).restart();
+  d.x = d.x;
+  d.y = d.y;
+}
+function dragged(d) {
+  d.x = d3.event.x;
+  d.y = d3.event.y;
+}
+function dragended(d) {
+  if (!d3.event.active) sim.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
