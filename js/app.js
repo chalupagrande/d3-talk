31913b65a3102442279615c43c86a9796b0c2d3c
Jamie Skinner <jamie@ninthmind.com>
2017-03-30 17:08:29 -0500

    added maps

--- js/app.js
+++ js/app.js
@@ -106,15 +106,15 @@ step()
 
 var nodes2;
 let five = d3.select('#svg-5')
-let width = 400
+let width = 600
 five.attrs({
   width: width,
   height: h
 })
 draw()
 let sim = d3.forceSimulation()
-            .force("charge", d3.forceManyBody().strength(5))
-            .force('collide', d3.forceCollide(d => d.r + 5))
+            .force("charge", d3.forceManyBody().strength(10))
+            .force('collide', d3.forceCollide(d => d.r * 1.5 + 5))
             .force("center", d3.forceCenter(width / 2, h / 2))
             .nodes(data)  // <= important
 
@@ -132,7 +132,7 @@ function tick(){
 function draw(){
   nodes2 = five.selectAll('.node')
               .data(data).enter().append('circle').attrs({
-                r: d => d.r,
+                r: d => d.r * 1.5,
                 fill: (d,i) => colorScale(i),
                 x: d => Math.random() * width,
                 y: d => Math.random() * h,
@@ -157,4 +157,66 @@ function dragended(d) {
   if (!d3.event.active) sim.alphaTarget(0);
   d.fx = null;
   d.fy = null;
+}
+
+
+//MAP
+let map = d3.select('#svg-6')
+let mapw = 900
+let maph = 500
+let worldGroup, projection, path, tempworld, graticule;
+d3.json("js/world-topo-min.json", (error, world) => {
+  if(error) {
+    throw new Error("Chrome has strict security permissions and won't allow you to fetch from a local file system. Use `http-server` to serve these files over HTTP to make the map visualization to work.")
+    d3.select('.err').style('display', 'block')
+  } else {
+    mapSetup(world)
+    tempworld = world
+  }
+});
+
+function mapSetup(world){
+    let latlonlines = map.append('g').attr('class','latlonlines')
+  worldGroup = map.append('g').attr('class','world')
+  graticule = d3.geoGraticule()
+                .step([10,10])
+  projection = d3.geoMercator()
+                // .geoOrthographic()
+      // .scale(mapw / 2 / Math.PI)
+      // .translate([mapw / 1.8, maph / 2])
+      // .scale(100)
+  var countries = topojson.feature(world, world.objects.countries)
+  colorScale.domain([0,countries.features.length]).range(['yellow','red'])
+  path = d3.geoPath()
+              .projection(projection);
+  country = worldGroup.selectAll('.country')
+          .data(countries.features)
+          .enter()
+          .append('path')
+          .attrs({
+            class: 'country',
+            d: path,
+            id: d => d.id,
+            title: d => d.properties.name,
+            fill: d => colorScale(d.id),
+          })
+          .on('mouseenter', (d)=>{
+            d3.select(d3.event.target)
+              .transition()
+              .attr('fill','white')
+          })
+          .on('mouseleave', ()=>{
+            d3.select(d3.event.target)
+              .transition()
+              .attr('fill',d => colorScale(d.id))
+          })
+
+  let grat = latlonlines.selectAll('path.graticule')
+              .data([graticule()])
+              .enter()
+              .append('path')
+              .attrs({
+                'd': path,
+                stroke: 'rgba(255,255,255,0.3)',
+              })
 }
